db7e2d7d1dfd36c7883b4503270a440b039123ec
wcxdell <wcxdell@163.com>
2015-08-25 16:52:08 +0800

    将textField和textView的边框样式变成一样

--- noteBook/ViewController.h
+++ noteBook/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UITextView *textView;
+@property (strong, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
--- noteBook/ViewController.m
+++ noteBook/ViewController.m
@@ -18,11 +18,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [UIApplication sharedApplication].statusBarHidden = YES;
+    
+    [self initWithText];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void) initWithText{
+    self.textView.layer.borderWidth = 0.5;
+    self.textView.layer.borderColor = [[UIColor lightGrayColor] CGColor];
+    self.textView.layer.cornerRadius = 2.0;
+    
+    self.textField.layer.borderColor = [[UIColor lightGrayColor] CGColor];
+    self.textField.layer.borderWidth = 0.5;
+    self.textField.layer.cornerRadius = 2.0;
 }
 
 -(BOOL) prefersStatusBarHidden{
