57aa7e053125322d15c7cad166d50cd02930173a
wcxdell <wcxdell@163.com>
2015-08-25 16:12:24 +0800

    create

---
+++ noteBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  noteBook
+//
+//  Created by 王长旭 on 15/8/25.
+//  Copyright (c) 2015年 王长旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ noteBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  noteBook
+//
+//  Created by 王长旭 on 15/8/25.
+//  Copyright (c) 2015年 王长旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
